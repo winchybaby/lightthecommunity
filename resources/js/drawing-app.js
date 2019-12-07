3bee8abd1bf2e76fc305ecd69033792aa8320871
Trent Sellers <sellerstj@bellsouth.net>
2019-12-07 17:20:48 -0500

    Made changes to canvas-related applications and formatting.

--- resources/js/drawing-app.js
+++ resources/js/drawing-app.js
@@ -2,14 +2,20 @@
 
 (function() {
 
-  var canvas = document.getElementById('letter');
-//   var colors = document.getElementsByClassName('color');
+  var canvas = document.getElementById('letter-canvas');
+  var colors = document.getElementsByClassName('color');
   var context = canvas.getContext('2d');
 
+  var defaultColor = 'black';
+  var defaultSize = 4;
+  var eraserSize = 150;
+
   var current = {
-    color: 'black'
+    color: defaultColor,
+    size: defaultSize
   };
   var drawing = false;
+  var verticalOffset = 64;
 
   canvas.addEventListener('mousedown', onMouseDown, false);
   canvas.addEventListener('mouseup', onMouseUp, false);
@@ -22,20 +28,19 @@
   canvas.addEventListener('touchcancel', onMouseUp, false);
   canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
 
-//   for (var i = 0; i < colors.length; i++){
-//     colors[i].addEventListener('click', onColorUpdate, false);
-//   }
+  for (var i = 0; i < colors.length; i++){
+    colors[i].addEventListener('click', onColorUpdate, false);
+  }
 
   window.addEventListener('resize', onResize, false);
   onResize();
 
-
-  function drawLine(x0, y0, x1, y1, color){
+  function drawLine(x0, y0, x1, y1, color, size){
     context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
+    context.moveTo(x0, y0 - verticalOffset);
+    context.lineTo(x1, y1 - verticalOffset);
     context.strokeStyle = color;
-    context.lineWidth = 2;
+    context.lineWidth = size;
     context.stroke();
     context.closePath();
 
@@ -52,19 +57,27 @@
   function onMouseUp(e){
     if (!drawing) { return; }
     drawing = false;
-    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color);
+    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, current.size);
   }
 
   function onMouseMove(e){
     if (!drawing) { return; }
-    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color);
+    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, current.size);
     current.x = e.clientX||e.touches[0].clientX;
     current.y = e.clientY||e.touches[0].clientY;
   }
 
-//   function onColorUpdate(e){
-//     current.color = e.target.className.split(' ')[1];
-//   }
+  function onColorUpdate(e){
+    current.color = e.target.className.split(' ')[1];
+    if (current.color == 'white')
+    {
+      current.size = eraserSize;
+    }
+    else
+    {
+      current.size = defaultSize;
+    }
+  }
 
   // limit the number of events per second
   function throttle(callback, delay) {
